ce96de53abe4af7df82f3340e83f32d8ced0eb18
kirubel tabu <kirubelderessa6@gmail.com>
2022-08-20 12:49:20 +0300

    Added icon and splash screen

--- TradingApp/TradingApp.Android/MainActivity.cs
+++ TradingApp/TradingApp.Android/MainActivity.cs
@@ -13,9 +13,11 @@ using TradingApp.Droid;
 [assembly: Dependency(typeof(Toaster))]
 namespace TradingApp.Droid
 {
-    [Activity(Label = "TradingApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "TradingApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private long lastPress;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,11 +26,36 @@ namespace TradingApp.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override void OnBackPressed()
+        {
+            int NavStackCount = App.Current.MainPage.Navigation.NavigationStack.Count;
+
+            if (NavStackCount == 1)
+            {
+                long currentTime = DateTime.UtcNow.Ticks / TimeSpan.TicksPerMillisecond;
+
+                if (currentTime - lastPress > 5000)
+                {
+                    Toast.MakeText(this, "Press back again to exit", ToastLength.Long).Show();
+                    lastPress = currentTime;
+                }
+                else
+                {
+                    base.OnBackPressed();
+                }
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
         }
     }
 
---
+++ TradingApp/TradingApp.Android/SplashActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using TradingApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace TradingApp.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        void SimulateStartup()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        }
+    }
+}
